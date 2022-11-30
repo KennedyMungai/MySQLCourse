b89309731b27e46aa23e6d2dac064e5daf5c499e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-30 08:49:08 +0300

    Created a new cats table

--- MySQLScripts.sql
+++ MySQLScripts.sql
@@ -1,3 +1,6 @@
 -- Active: 1669786832810@@127.0.0.1@3306@CatnipMOFO
-SELECT *
-FROM cat;
+CREATE TABLE cats (
+    cat_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    age INT,
+);
