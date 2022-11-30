da778c91ab63a0f6e72d54b0d0b496451df162be
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-30 09:03:30 +0300

    Created the books table

---
+++ book_data.sql
@@ -0,0 +1,9 @@
+CREATE TABLE books (
+    book_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100),
+    author_fname VARCHAR(100),
+    author_lname VARCHAR(100),
+    released_year INT,
+    stock_quantity INT,
+    pages INT
+);
