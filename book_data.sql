73d263047985c1fecbdd266a2477a1b1ef1a643b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-01 15:51:54 +0300

    Replaced the query statement

--- book_data.sql
+++ book_data.sql
@@ -1,8 +1,9 @@
 -- Active: 1669874133351@@127.0.0.1@3306@books_db
 SELECT title,
-    released_year,
+    stock_quantity,
     CASE
-        WHEN released_year >= 2000 THEN 'Modern Lit'
-        ELSE '20th Century Lit'
-    END AS 'Genre'
+        WHEN stock_quantity BETWEEN 0 AND 50 THEN '*'
+        WHEN stock_quantity BETWEEN 50 AND 100 THEN '**'
+        ELSE '***'
+    END AS 'Stock'
 FROM books
