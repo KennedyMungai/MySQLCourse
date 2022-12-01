574f876aba9fab307d06b51d838b52ad979a539e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-01 09:54:13 +0300

    Inserted two rows of sample data

--- people.sql
+++ people.sql
@@ -1,6 +1,22 @@
+-- Active: 1669874133351@@127.0.0.1@3306@dates_and_times_db
 CREATE TABLE people (
     name VARCHAR(100),
     birthdate DATE,
     birthtime TIME,
     birthday DATETIME
 );
+-- Inserting values into the table
+-- -------------------------------
+INSERT INTO people(name, birthdate, birthtime, birthday)
+VALUES (
+        'Padma',
+        '1983-11-11',
+        '10:07:35',
+        '1983-1-1 10:07:35'
+    ),
+    (
+        'Larry',
+        '1943-12-25',
+        '04:10:42',
+        '1943-12-25 04:10:42'
+    );
