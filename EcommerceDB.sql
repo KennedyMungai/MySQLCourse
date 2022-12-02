8d3bd3962d51994a02a0da6256435b741d552742
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-02 09:32:29 +0300

    Commented out the orders data

--- EcommerceDB.sql
+++ EcommerceDB.sql
@@ -28,8 +28,8 @@
 --     ('David', 'Bowie', 'david@gmail.com'),
 --     ('Blue', 'Steele', 'blue@gmail.com'),
 --     ('Bette', 'Davis', 'bette@gmail.com');
-INSERT INTO orders (order_date, amount, customer_id)
-VALUES ('2016-02-10', 99.99, 1),
-    ('2017-11-11', 35.50, 1),
-    ('2014-12-12', 800.67, 2),
-    ('2015-01-03', 12.50, 2);
+-- INSERT INTO orders (order_date, amount, customer_id)
+-- VALUES ('2016-02-10', 99.99, 1),
+--     ('2017-11-11', 35.50, 1),
+--     ('2014-12-12', 800.67, 2),
+--     ('2015-01-03', 12.50, 2);
