ddf8ca8be615bca35cfc634f99306dda618e2f35
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-02 09:24:05 +0300

    Added insert statements for the two newly created tables

--- EcommerceDB.sql
+++ EcommerceDB.sql
@@ -11,3 +11,14 @@
 --     amount DECIMAL(10, 2) NOT NULL,
 --     customer_id INT NOT NULL
 -- );
+INSERT INTO customers(first_name, last_name, email)
+VALUES ('Boy', 'George', 'george@gmail.com'),
+    ('George', 'Michael', 'gm@gmail.com'),
+    ('David', 'Bowie', 'david@gmail.com'),
+    ('Blue', 'Steele', 'blue@gmail.com'),
+    ('Bette', 'Davis', 'bette@gmail.com');
+INSERT INTO orders (order_date, amount, customer_id)
+VALUES ('2016-02-10', 99.99, 1),
+    ('2017-11-11', 35.50, 1),
+    ('2014-12-12', 800.67, 2),
+    ('2015-01-03', 12.50, 2);
