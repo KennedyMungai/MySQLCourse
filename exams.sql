da4dbc8cd0fff4144b9d6d4a41832ad3be20a5d6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-04 12:21:47 +0300

    Added a query to drop the tables

--- exams.sql
+++ exams.sql
@@ -33,3 +33,27 @@
 -- SELECT *
 -- FROM students,
 --     papers;
+-----------------------------------------------------
+-----------------------------------------------------
+-- Inserting the information to the various tables
+-----------------------------------------------------
+-----------------------------------------------------
+-- INSERT INTO students(first_name)
+-- VALUES ('Caleb'),
+--     ('Samantha'),
+--     ('Raj'),
+--     ('Carlos'),
+--     ('Lisa');
+-- INSERT INTO papers(student_id, title, grade)
+-- VALUES (1, 'My first book report', 60),
+--     (1, 'My second book report', 75),
+--     (2, 'Russian lit through the ages', 94),
+--     (2, 'De Montaigne and The art of the essay', 98),
+--     (4, 'Borges and magical realism', 89);
+-----------------------------------------------------
+-----------------------------------------------------
+-- Deleting the tables
+-----------------------------------------------------
+-----------------------------------------------------
+-- DROP TABLES students,
+-- papers;
