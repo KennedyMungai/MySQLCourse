6154e3403c967ac115c4b4b7fbe94d9cb941b696
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-04 12:42:38 +0300

    Used the proper syntax

--- exams.sql
+++ exams.sql
@@ -16,16 +16,16 @@
 -- Create the tables
 -----------------------------------------------------
 -----------------------------------------------------
-/* CREATE TABLE IF NOT EXISTS students (
- id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
- first_name VARCHAR(50) NOT NULL
- ); */
-/* CREATE TABLE IF NOT EXISTS papers (
- title VARCHAR(50) NOT NULL PRIMARY KEY,
- grade INT NOT NULL,
- student_id INT NOT NULL,
- FOREIGN KEY(student_id) REFERENCES students(id)
- ); */
+-- CREATE TABLE IF NOT EXISTS students (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     first_name VARCHAR(50) NOT NULL
+-- );
+-- CREATE TABLE IF NOT EXISTS papers (
+--     title VARCHAR(50) NOT NULL PRIMARY KEY,
+--     grade INT NOT NULL,
+--     student_id INT NOT NULL,
+--     FOREIGN KEY(student_id) REFERENCES students(id)
+-- );
 -----------------------------------------------------
 -----------------------------------------------------
 -- Select statement
