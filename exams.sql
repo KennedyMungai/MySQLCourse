8a2c4deb19744e8eb1a99fb23559d0a80fda57e8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-04 12:38:54 +0300

    Commented out the sql statements

--- exams.sql
+++ exams.sql
@@ -1,55 +1,56 @@
+-- Active: 1670146345366@@127.0.0.1@3306@exams_db
 -----------------------------------------------------
 -----------------------------------------------------
 -- Create the Database
 -----------------------------------------------------
 -----------------------------------------------------
-CREATE DATABASE IF NOT EXISTS exams_db;
+/* CREATE DATABASE IF NOT EXISTS exams_db; */
 -----------------------------------------------------
 -----------------------------------------------------
 -- Use the database
 -----------------------------------------------------
 -----------------------------------------------------
-USE exams_db;
+/* USE exams_db; */
 -----------------------------------------------------
 -----------------------------------------------------
 -- Create the tables
 -----------------------------------------------------
 -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS students (
+/* CREATE TABLE IF NOT EXISTS students (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL
-);
-CREATE TABLE IF NOT EXISTS papers (
+); */
+/* CREATE TABLE IF NOT EXISTS papers (
     title VARCHAR(50) NOT NULL PRIMARY KEY,
     grade INT NOT NULL,
     student_id INT NOT NULL,
     FOREIGN KEY(student_id) REFERENCES students(id)
-);
+); */
 -----------------------------------------------------
 -----------------------------------------------------
 -- Select statement
 -----------------------------------------------------
 -----------------------------------------------------
--- SELECT *
--- FROM students,
---     papers;
+/* SELECT *
+FROM students,
+    papers; */
 -----------------------------------------------------
 -----------------------------------------------------
 -- Inserting the information to the various tables
 -----------------------------------------------------
 -----------------------------------------------------
--- INSERT INTO students(first_name)
--- VALUES ('Caleb'),
---     ('Samantha'),
---     ('Raj'),
---     ('Carlos'),
---     ('Lisa');
--- INSERT INTO papers(student_id, title, grade)
--- VALUES (1, 'My first book report', 60),
---     (1, 'My second book report', 75),
---     (2, 'Russian lit through the ages', 94),
---     (2, 'De Montaigne and The art of the essay', 98),
---     (4, 'Borges and magical realism', 89);
+/* INSERT INTO students(first_name) */
+/* VALUES ('Caleb'), */
+    /* ('Samantha'), */
+    /* ('Raj'), */
+    /* ('Carlos'), */
+    /* ('Lisa'); */
+/* INSERT INTO papers(student_id, title, grade) */
+/* VALUES (1, 'My first book report', 60), */
+    /* (1, 'My second book report', 75), */
+    /* (2, 'Russian lit through the ages', 94), */
+    /* (2, 'De Montaigne and The art of the essay', 98), */
+    /* (4, 'Borges and magical realism', 89); */
 -----------------------------------------------------
 -----------------------------------------------------
 -- Deleting the tables
