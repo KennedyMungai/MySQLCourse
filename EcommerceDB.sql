cba9586953ef226eaddd34c9b916fa9e3a7437f8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-04 10:33:25 +0300

    Ordered the results by the total amount of money spent

--- EcommerceDB.sql
+++ EcommerceDB.sql
@@ -76,7 +76,8 @@
 -- ------------------------------------------------
 SELECT first_name,
     last_name,
-    IFNULL(SUM(amount), 0) AS 'Total Spent'
+    IFNULL(SUM(amount), 0) AS total_spent
 FROM customers
     LEFT JOIN orders ON customers.id = orders.customer_id;
-GROUP BY customers.id;
+GROUP BY customers.id
+ORDER BY total_spent;
