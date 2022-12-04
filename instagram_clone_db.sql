37e15c5cf98dce4f612d7519f52075535a8f3cb3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-04 20:19:48 +0300

    Created the users table

--- instagram_clone_db.sql
+++ instagram_clone_db.sql
@@ -1,1 +1,11 @@
 -- Active: 1670173412614@@127.0.0.1@3306@instagram_clone_db
+-- -------------------------------------------------------------
+-- -------------------------------------------------------------
+-- Creating tables
+-- -------------------------------------------------------------
+-- -------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT NOW()
+);
