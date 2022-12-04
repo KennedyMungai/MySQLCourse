1ec2bd9080b072ddf3095bbb6edc04ed2a666e01
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-04 12:33:32 +0300

    Updated the create table query

--- exams.sql
+++ exams.sql
@@ -3,28 +3,28 @@
 -- Create the Database
 -----------------------------------------------------
 -----------------------------------------------------
--- CREATE DATABASE IF NOT EXISTS exams_db;
+CREATE DATABASE IF NOT EXISTS exams_db;
 -----------------------------------------------------
 -----------------------------------------------------
 -- Use the database
 -----------------------------------------------------
 -----------------------------------------------------
--- USE exams_db;
+USE exams_db;
 -----------------------------------------------------
 -----------------------------------------------------
 -- Create the tables
 -----------------------------------------------------
 -----------------------------------------------------
--- CREATE TABLE IF NOT EXISTS students (
---     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     first_name VARCHAR(50) NOT NULL
--- );
--- CREATE TABLE IF NOT EXISTS papers (
---     title VARCHAR(50) NOT NULL PRIMARY KEY,
---     grade CHAR NOT NULL,
---     student_id INT NOT NULL,
---     FOREIGN KEY(student_id) REFERENCES students(id)
--- );
+CREATE TABLE IF NOT EXISTS students (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS papers (
+    title VARCHAR(50) NOT NULL PRIMARY KEY,
+    grade INT NOT NULL,
+    student_id INT NOT NULL,
+    FOREIGN KEY(student_id) REFERENCES students(id)
+);
 -----------------------------------------------------
 -----------------------------------------------------
 -- Select statement
