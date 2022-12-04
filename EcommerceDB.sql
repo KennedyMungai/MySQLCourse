ea8a1a88dcbd0b86c361cf0a417d9e3033c50f00
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-04 09:56:58 +0300

    Added a join that is sorted by the amount

--- EcommerceDB.sql
+++ EcommerceDB.sql
@@ -38,9 +38,21 @@
 -- Explicit Inner Join
 -- ------------------------------------------------
 -- ------------------------------------------------
+-- SELECT first_name,
+--     last_name,
+--     order_date,
+--     amount
+-- FROM customers
+--     JOIN orders ON customers.id = orders.customer_id
+-- ------------------------------------------------
+-- ------------------------------------------------
+-- A fancier Join 
+-- ------------------------------------------------
+-- ------------------------------------------------
 SELECT first_name,
     last_name,
     order_date,
     amount
 FROM customers
     JOIN orders ON customers.id = orders.customer_id
+ORDER BY amount;
