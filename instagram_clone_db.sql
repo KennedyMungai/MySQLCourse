7b73cdee02b9bcc4b93fa8873a7bdbb203929292
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-04 20:54:52 +0300

    Made the combination of user_id and photo_id a prmary key

--- instagram_clone_db.sql
+++ instagram_clone_db.sql
@@ -37,7 +37,8 @@ CREATE TABLE IF NOT EXISTS likes (
     photo_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
     FOREIGN KEY(user_id) REFERENCES users(id),
-    FOREIGN KEY(photo_id) REFERENCES photos(id)
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    PRIMARY KEY (user_id, photo_id)
 );
 -- -------------------------------------------------------------
 -- -------------------------------------------------------------
