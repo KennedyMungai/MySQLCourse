54831959d67ac7221796ac5d136d3519f93f3e5a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-04 20:34:15 +0300

    Created the photos table

--- instagram_clone_db.sql
+++ instagram_clone_db.sql
@@ -16,6 +16,13 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(30) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
+CREATE TABLE IF NOT EXISTS photos (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL UNIQUE,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
 -- -------------------------------------------------------------
 -- -------------------------------------------------------------
 -- Alter statements
