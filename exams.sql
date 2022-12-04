a455e2a2d4b8cc4ceef762928e4d522693946445
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-04 12:41:37 +0300

    Took care of an error

--- exams.sql
+++ exams.sql
@@ -17,40 +17,40 @@
 -----------------------------------------------------
 -----------------------------------------------------
 /* CREATE TABLE IF NOT EXISTS students (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL
-); */
+ id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ first_name VARCHAR(50) NOT NULL
+ ); */
 /* CREATE TABLE IF NOT EXISTS papers (
-    title VARCHAR(50) NOT NULL PRIMARY KEY,
-    grade INT NOT NULL,
-    student_id INT NOT NULL,
-    FOREIGN KEY(student_id) REFERENCES students(id)
-); */
+ title VARCHAR(50) NOT NULL PRIMARY KEY,
+ grade INT NOT NULL,
+ student_id INT NOT NULL,
+ FOREIGN KEY(student_id) REFERENCES students(id)
+ ); */
 -----------------------------------------------------
 -----------------------------------------------------
 -- Select statement
 -----------------------------------------------------
 -----------------------------------------------------
 /* SELECT *
-FROM students,
-    papers; */
+ FROM students,
+ papers; */
 -----------------------------------------------------
 -----------------------------------------------------
 -- Inserting the information to the various tables
 -----------------------------------------------------
 -----------------------------------------------------
-/* INSERT INTO students(first_name) */
-/* VALUES ('Caleb'), */
-    /* ('Samantha'), */
-    /* ('Raj'), */
-    /* ('Carlos'), */
-    /* ('Lisa'); */
-/* INSERT INTO papers(student_id, title, grade) */
-/* VALUES (1, 'My first book report', 60), */
-    /* (1, 'My second book report', 75), */
-    /* (2, 'Russian lit through the ages', 94), */
-    /* (2, 'De Montaigne and The art of the essay', 98), */
-    /* (4, 'Borges and magical realism', 89); */
+-- INSERT INTO students(first_name)
+-- VALUES ('Caleb'),
+--     ('Samantha'),
+--     ('Raj'),
+--     ('Carlos'),
+--     ('Lisa');
+-- INSERT INTO papers(student_id, title, grade)
+-- VALUES (1, 'My first book report', 60),
+--     (1, 'My second book report', 75),
+--     (2, 'Russian lit through the ages', 94),
+--     (2, 'De Montaigne and The art of the essay', 98),
+--     (4, 'Borges and magical realism', 89);
 -----------------------------------------------------
 -----------------------------------------------------
 -- Deleting the tables
