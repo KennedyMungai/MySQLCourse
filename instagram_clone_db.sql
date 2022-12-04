4a290929613b9d7897abc0fe26808931578a8b75
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-04 21:12:09 +0300

    Made the photo_id and the tag_id the primary key of the table

--- instagram_clone_db.sql
+++ instagram_clone_db.sql
@@ -57,7 +57,8 @@ CREATE TABLE IF NOT EXISTS photo_tags (
     photo_id INT NOT NULL,
     tag_id INT NOT NULL,
     FOREIGN KEY (photo_id) REFERENCES photos(id),
-    FOREIGN KEY (tag_id) REFERENCES tags(id)
+    FOREIGN KEY (tag_id) REFERENCES tags(id),
+    PRIMARY KEY (photo_id, tag_id)
 );
 -- -------------------------------------------------------------
 -- -------------------------------------------------------------
