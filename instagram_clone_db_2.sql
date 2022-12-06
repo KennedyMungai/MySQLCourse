cad3e1458e4f4295b5c8e8583c2a4bbcd8761cdd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-06 11:41:58 +0300

    Findiing the most popular sign on days of the week

--- instagram_clone_db_2.sql
+++ instagram_clone_db_2.sql
@@ -14,3 +14,14 @@ SELECT *
 FROM users
 ORDER BY created_at DESC
 LIMIT 5;
+-- --------------------------------------
+-- --------------------------------------
+-- A query to find out on what day of 
+-- the week most users register on
+-- --------------------------------------
+-- --------------------------------------
+SELECT DAYNAME(created_at) AS day,
+    COUNT(*) AS count
+FROM users
+GROUP BY day
+ORDER BY count DESC;
